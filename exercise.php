d16d3689e328d2dc8a74f7a8d7143f6d7f7851f8
ajwulf <ajwulf.13@gmail.com>
2015-10-06 15:31:46 -0700

    exercise added

---
+++ exercise.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+      <?php
+        <!-- function nameday($first, $last) {
+        echo "My name is $first $last .";
+        }
+        nameday("John","Doe");
+         -->
+        function twoint()
+
+
+
+        <!-- function integ($n) {
+          while ($i < $n) {
+            $i++ ;
+            echo $n;
+          }
+        }
+        integ(5); -->
+      ?>
+  </body>
+</html>
